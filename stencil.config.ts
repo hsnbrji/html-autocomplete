3992a382e0ffa2f0f555d4a7c3b9aeac7ef5345e
Hassan Bourji <Hassan.bourji@pin-pay.com>
2020-05-17 08:35:03 +0300

    feat: re-added all browser support

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,16 +4,9 @@ export const config: Config = {
   namespace: 'html-autocomplete',
   globalScript: './src/globals/global.ts',
   taskQueue: 'async',
-  buildEs5: false,
+  // buildEs5: false,
   hashFileNames: true,
-  enableCache: true,
-  extras: {
-    cssVarsShim: false,
-    dynamicImportShim: false,
-    safari10: false,
-    scriptDataOpts: false,
-    shadowDomShim: false
-  },
+ 
   outputTargets: [
     {
       type: 'dist',
